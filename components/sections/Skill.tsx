8afd8d53a995de5a4aa78be9b4a8c2bae8636d95
yliess86 <hatiyliess86@gmail.com>
2023-01-16 10:56:30 +0100

    [Feature] Removed Useless

--- components/sections/Skill.tsx
+++ components/sections/Skill.tsx
@@ -6,12 +6,12 @@ import Badge from "../elements/Badge";
 
 export default function Skills ({ skills }: { skills: ISkill[] }) {
   const elements = skills.map((skill, index) =>
-    <div key={index} className="flex flex-col md:grid md:grid-cols-2 gap-4">
-      <Card className={`flex flex-col gap-2 ${index % 2 == 0 ? "md:order-first" : "md:order-last" }`}>
+    <div key={index} className="flex flex-col md:grid md:grid-cols-10 gap-4">
+      <Card className={`flex flex-col md:col-span-6 gap-2 ${index % 2 == 0 ? "md:order-first" : "md:order-last" }`}>
         <h3 className="font-bold text-xl">{skill.domain}</h3>
         <p>{skill.desc}</p>
       </Card>
-      <div className={`flex flex-wrap justify-start content-start gap-4 ${index % 2 == 0 ? "md:order-last" : "md:order-first" }`}>
+      <div className={`flex flex-wrap justify-start content-start md:col-span-4 gap-4 ${index % 2 == 0 ? "md:order-last" : "md:order-first" }`}>
         {skill.items.map((item, index) => <Badge key={index} name={item} />)}
       </div>
     </div>
