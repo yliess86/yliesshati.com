ddd1c1ce682001fcdbb99a9e87bde0d8e2f86a02
yliess86 <hatiyliess86@gmail.com>
2023-01-16 11:02:58 +0100

    [Fix] Resource export

--- next.config.js
+++ next.config.js
@@ -2,10 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: './',
-  images: {
-    unoptimized: true,
-  },
+  assetPrefix: '.',
+  images: { unoptimized: true },
 }
 
 module.exports = nextConfig
