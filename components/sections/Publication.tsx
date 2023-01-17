709560580b03c16b02cdf5a68ddf8eac7efff402
yliess86 <hatiyliess86@gmail.com>
2023-01-17 16:03:18 +0100

    [Feature] Improved Abstratc Length for Lisibility

--- components/sections/Publication.tsx
+++ components/sections/Publication.tsx
@@ -8,12 +8,12 @@ import ExternalLink from "../elements/ExternalLink";
 export default function Awards ({ publications }: { publications: IPublication[] }) {
   const elements = publications.map((publication, index) =>
     <Card key={index} className="flex flex-col gap-2">
-      <div className="overflow-hidden rounded-sm w-full h-96">
+      <div className="overflow-hidden rounded-sm w-full h-64 md:h-96">
         <Image className="object-cover w-full h-full" alt={`cover ${publication.title}`} src={publication.cover} width={256} height={256} />
       </div>
       <h3 className="font-bold text-xl">{publication.title}</h3>
       <h4 className="flex items-center"><FaMapMarker className="inline mr-1" />{publication.conference} - {publication.year}</h4>
-      <h4 className="flex items-center gap-2"><FaUserFriends className="inline mr-1" />{publication.authors.map((author, index) => <span key={index}>{author.name} {author.surname}</span>)}</h4>
+      <h4 className="flex items-center flex-wrap gap-2"><FaUserFriends className="inline mr-1" />{publication.authors.map((author, index) => <span key={index}>{author.name} {author.surname}</span>)}</h4>
       <ExternalLink href={`https://doi.org/${publication.doi}`}><h4 className="flex items-center md:mb-4"><FaLink className="inline mr-1" />{publication.doi}</h4></ExternalLink>
       <p>{publication.abstract}</p>
     </Card>
