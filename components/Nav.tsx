e4732674beb03b445877120cf4463b705a343da9
yliess86 <hatiyliess86@gmail.com>
2023-01-19 23:34:28 +0100

    [Fixed] Go to Top (useRouter)

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { FaArrowUp } from "react-icons/fa"
+import { useRouter } from "next/router";
 
 export default function Nav() {
   return (
@@ -16,7 +17,7 @@ export default function Nav() {
         <Link href="/#awards" scroll={false}>Awards</Link>
         <Link href="/#education" scroll={false}>Education</Link>
       </div>
-      <Link href="/" className="fixed bottom-6 right-6">
+      <Link href={useRouter().asPath} className="fixed bottom-6 right-6 z-50">
         <div className="rounded-full w-10 h-10 bg-black"><FaArrowUp className="text-white m-auto h-10" /></div>
       </Link>
     </nav>
