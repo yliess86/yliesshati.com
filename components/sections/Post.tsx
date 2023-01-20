4871debe690efa0709ac1ca9d558b249684bc023
yliess86 <hatiyliess86@gmail.com>
2023-01-20 01:05:54 +0100

    [Feature] Changed header Post Metadata Position

--- components/sections/Post.tsx
+++ components/sections/Post.tsx
@@ -11,15 +11,17 @@ export default function Post ({ post }: { post: IPost }) {
   return (
     <section className="flex flex-col gap-4 md:gap-6">
       <Card className="flex flex-col text-left gap-6">
-        <div className="flex justify-between text-sm md:text-base">
-          <Badge name={post.infos.metadata.type} />
-          <div><FaCalendarDay className="inline mr-1" />{ post.infos.metadata.date }</div>
-          <div><FaClock className="inline mr-1"/>{post.infos.metadata.min} min</div>
-        </div>
         <div className="border border-black rounded-sm overflow-hidden w-full h-48 sm:h-96 mx-auto">
           <Image className="object-cover w-full h-full" alt={post.infos.metadata.title} src={post.infos.metadata.cover} width={256} height={256} />
         </div>
         <h1 className="font-black text-2xl md:text-4xl text-center">{ post.infos.metadata.title }</h1>
+        <div className="flex justify-between text-sm md:text-base">
+          <Badge name={post.infos.metadata.type} />
+          <div className="flex flex-row gap-2">
+            <div className="flex items-center"><FaCalendarDay className="inline mr-1" />{ post.infos.metadata.date }</div>
+            <div className="flex items-center"><FaClock className="inline mr-1"/>{post.infos.metadata.min} min</div>
+          </div>
+        </div>
       </Card>
       <article dangerouslySetInnerHTML={{ __html: marked(post.content) }} />
     </section>
